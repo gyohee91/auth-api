7a30ed3a3b6596c34d5d4b7bb58e21a39397b4ea
gyohee <gyohee91@gmail.com>
2026-02-04 17:29:10 +0900

    Kafka 로컬 세팅으로 인한 소스 변경

--- src/main/java/com/okbank/fintech/domain/notification/kafka/producer/NotificationEventProducer.java
+++ src/main/java/com/okbank/fintech/domain/notification/kafka/producer/NotificationEventProducer.java
@@ -5,6 +5,7 @@ import com.okbank.fintech.global.util.RequestIdPropagator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -15,20 +16,37 @@ public class NotificationEventProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final RequestIdPropagator requestIdPropagator;
 
+    private static final String TOPIC_NOTIFICATION_CREATED = "notification.created";
+
     public void publishNotificationCreated(NotificationCreatedEvent event) {
-        String topic = "notification.created";
         String key = event.getNotificationId().toString();
 
-        ProducerRecord<String, Object> record = new ProducerRecord<>(topic, key, event);
+        ProducerRecord<String, Object> record = new ProducerRecord<>(
+                TOPIC_NOTIFICATION_CREATED,
+                null,
+                key,
+                event
+        );
 
         //requestId 전파
         requestIdPropagator.propagateToKafka(record);
 
         try {
             kafkaTemplate.send(record)
-                    .whenComplete((result, throwable) -> {
-                        log.info("Successfully published event to topic: {}, key: {}, partition: {}",
-                                topic, key, result.getRecordMetadata().partition());
+                    .thenApply(result -> {
+                        RecordMetadata metadata = result.getRecordMetadata();
+                        log.info("Successfully published: id={}, key={}, partition={}, offset={}",
+                                event.getNotificationId(),
+                                key,
+                                metadata.partition(),
+                                metadata.offset()
+                        );
+                        return result;
+                    })
+                    .exceptionally(ex ->{
+                        log.error("Failed to publish notification: id={}, key: {}",
+                                event.getNotificationId(), key, ex);
+                        throw new RuntimeException(ex);
                     });
         } catch (Exception e) {
             log.error("Error while sending event to kafka", e);
--- src/main/java/com/okbank/fintech/global/config/KafkaTopicConfig.java
+++ src/main/java/com/okbank/fintech/global/config/KafkaTopicConfig.java
@@ -1,12 +1,26 @@
 package com.okbank.fintech.global.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
+    @Bean
+    public KafkaAdmin kafkaAdmin(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        return new KafkaAdmin(configs);
+    }
+
     @Bean
     public NewTopic notificationCreatedTopic() {
         return TopicBuilder.name("notification.created")
