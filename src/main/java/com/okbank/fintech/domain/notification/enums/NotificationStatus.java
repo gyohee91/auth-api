91136e683630e53432a4a36f632dccbeeb962edb
gyohee <gyohee91@gmail.com>
2026-04-01 11:16:41 +0900

    참조 안하는 domain 제거

--- src/main/java/com/okbank/fintech/domain/notification/enums/NotificationStatus.java
+++
@@ -1,8 +0,0 @@
-package com.okbank.fintech.domain.notification.enums;
-
-public enum NotificationStatus {
-    PENDING,    //생성됨
-    QUEUED,     //Kafka에 publish됨
-    SENT,       //발송 성공
-    FAILED      //발송 실패
-}
